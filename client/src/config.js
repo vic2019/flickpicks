0074e97e3c67636977d550f84007607e408a0701
Victor <>
2019-08-01 16:23:18 -0700

    Removed config

--- client/src/config.js
+++
@@ -1,6 +0,0 @@
-export const HOST = process.env.NODE_ENV === 'production'?
-  'https://api.victorwang.info/flickpicks': 'http://localhost:3009/flickpicks';
-
-export const img500BaseUrl = 'https://image.tmdb.org/t/p/w500';
-
-export const imgOriginalBaseUrl = 'https://image.tmdb.org/t/p/original';
