26e67bbd804845e714dabb50942798f2524a743e
Victor <>
2019-08-01 16:14:15 -0700

    Revert "Updated production config"
    This reverts commit 5d1135c7b6240f000654e573823fbc045ef99028.

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,7 @@
-export const HOST = process.env.NODE_ENV === 'production'?
-  'https://api.flickpicks.victorwang.info/': 'http://localhost:3009';
+// export const HOST = process.env.NODE_ENV === 'production'?
+//   'https://flickpicks.victorwang.info/api': 'http://localhost:3009/api';
+
+export const HOST = 'http://localhost:3009/api';
 
 export const img500BaseUrl = 'https://image.tmdb.org/t/p/w500';
 
