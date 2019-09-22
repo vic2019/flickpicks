02b998ee9da503177d830d2ec2debae3b2404bb4
Victor <>
2019-09-21 19:35:22 -0700

    Modified routing path

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ const origin = config.get('server').origin;
 
 app.use(cors({ origin }));
 
-app.use('/1/discover', require('./api/discover'));
-app.use('/1/movie', require('./api/movie-page'));
-app.use('/1/search', require('./api/search'));
+app.use('/api/discover', require('./api/discover'));
+app.use('/api/movie', require('./api/movie-page'));
+app.use('/api/search', require('./api/search'));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
