3f2127faf1d99812cb69ad76da17bdcaf3ce2f69
Victor <>
2019-10-01 06:40:24 -0700

    Added login button and drawer

--- client/src/components/TopNavBar.tsx
+++ client/src/components/TopNavBar.tsx
@@ -3,9 +3,13 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import InputBase from '@material-ui/core/InputBase';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Button from '@material-ui/core/Button'
 import { makeStyles } from '@material-ui/core/styles';
-// import IconButton from '@material-ui/core/IconButton';
-// import MenuIcon from '@material-ui/icons/Menu';
 
 import { NavLink } from 'react-router-dom';
 
@@ -17,8 +21,11 @@ const useStyles = makeStyles({
   root: {
     display: 'flex',
     justifyContent: 'space-between'
+  },
+  drawerList: {
+    width: 250,
   }
-})
+});
 
 interface Props {
   userAction: any,
@@ -27,27 +34,31 @@ interface Props {
 
 export default function TopNavBar({ userAction, email }: Props) {
   const classes = useStyles();
-  const handleClick = () => {
+  const [drawerOpen, setDrawer] = React.useState(false);
+
+  const handleUserAction = () => {
     userAction();
   };
+
+  const toggleDrawer = (isOpen: boolean) => () => setDrawer(isOpen);
 
   return (
     <>
       <AppBar className='TopNavBar'>
         <Toolbar
-          // disableGutters
+          disableGutters
           classes={{ root: classes.root }}
         >
-          {/* <IconButton aria-label='Open drawer'>
-            <MenuIcon style={{ visibility: 'hidden' }}/>
-          </IconButton> */}
-          <img
+          <IconButton aria-label='Open drawer'>
+            <MenuIcon onClick={toggleDrawer(true)}/>
+          </IconButton>
+          {/* <img
             className='tmdb-logo'
             src={tmdbLogo} 
             alt='Powered by tMDb' 
             style={{ width: '40px' }}
             title='This app uses the TMDb API but is not endorsed or certified by TMDb.' 
-          />
+          /> */}
           <Typography>
             <NavLink to='/mymovies' activeClassName='active-link'>
               My Movies
@@ -61,19 +72,54 @@ export default function TopNavBar({ userAction, email }: Props) {
               Friends
             </NavLink> */}
 
-            {/* ^^^ This part will be expanded upon in the future */}
+            {/* ^ This part will be expanded upon in the future */}
 
           </Typography>
-          <span onClick={handleClick}>
-            {email? `Logout from ${email.slice(0, 5)}..`: 'Login'}
-          </span> 
-          {/* ^^^This is to make the flex-box 'space-between' work */}
+          <span />
+          {/* ^This is to make the flex-box 'space-between' work */}
         </Toolbar>
         <SearchBar />
         <ProgressBar />
       </AppBar>
       <InputBase /> // This is here for spacing
       <Toolbar />
+      <SwipeableDrawer
+        open={drawerOpen}
+        onClose={toggleDrawer(false)}
+        onOpen={toggleDrawer(true)}
+      >
+        <div className={classes.drawerList} >
+          <List>
+            <ListItem>
+              <img
+                className='tmdb-logo'
+                src={tmdbLogo} 
+                alt='Powered by tMDb' 
+                style={{ width: '140px' }}
+                title='This app uses the TMDb API but is not endorsed or certified by TMDb.' 
+              />
+            </ListItem>
+            <ListItem>
+              <Button 
+                onClick={handleUserAction}
+                style={{ textTransform: 'none' }}
+                fullWidth
+                variant='contained'
+                color='primary'
+                size='small'
+              >
+                {email? 'Log out': 'Log in'}
+              </Button>
+            </ListItem>
+            {email? 
+              <ListItem><span style={{ overflow: 'hidden' }}>
+                {email}
+              </span></ListItem>:
+            null}
+          </List>
+        </div>
+      </SwipeableDrawer>
     </>
   );
 }
+
